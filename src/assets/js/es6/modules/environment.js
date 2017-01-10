1b89d82f62f4cc9bea84a8a75d525a3c22e20bd3
Mohamed hassan <pencilpix11@gmail.com>
2017-01-10 06:52:56 +0200

    basic level and elements object

---
+++ src/assets/js/es6/modules/environment.js
@@ -0,0 +1,17 @@
+export var level = [
+  'xxxxxxxxx',
+  'x       x',
+  'x       x',
+  'x =     x',
+  'x   -   x',
+  'x   --  x',
+  'x       x',
+  'x       x',
+  'xxxxxxxxx'
+];
+
+export var elements = {
+  wall: 'x',
+  food: '=',
+  player: '-'
+}
