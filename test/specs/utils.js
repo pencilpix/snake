ce1e86380ecb9efdf6a54d46aa6080235eaeda69
Mohamed hassan <pencilpix11@gmail.com>
2017-01-10 18:09:48 +0200

    UT: utils - addVector negative and zero return

--- test/specs/utils.js
+++ test/specs/utils.js
@@ -6,4 +6,16 @@ describe('addVector', () => {
     expect(v.x).toEqual(5);
     expect(v.y).toEqual(5);
   });
+
+  it('should return x = 0, y = 0', () => {
+    let v = addVector({x: 3, y: 4}, {x: -3, y: -4});
+    expect(v.x).toEqual(0);
+    expect(v.y).toEqual(0);
+  });
+
+  it('should return x = -1, y = -2', () => {
+    let v = addVector({x: 3, y: 4}, {x: -4, y: -6});
+    expect(v.x).toEqual(-1);
+    expect(v.y).toEqual(-2);
+  });
 });
