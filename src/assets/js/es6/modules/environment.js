21ee98fa41ac3174a664e7b817467a1a9fad6b13
Mohamed hassan <pencilpix11@gmail.com>
2017-01-10 14:48:24 +0200

    environment - directions object and fix the basic level

--- src/assets/js/es6/modules/environment.js
+++ src/assets/js/es6/modules/environment.js
@@ -1,17 +1,41 @@
+/**
+ * basic level simulation
+ * @type {Array}
+ */
 export var level = [
   'xxxxxxxxx',
   'x       x',
   'x       x',
-  'x =     x',
-  'x   -   x',
-  'x   --  x',
+  'x       x',
+  'x       x',
+  'x       x',
   'x       x',
   'x       x',
   'xxxxxxxxx'
 ];
 
+
+
+/**
+ * environment elements
+ * @type {Object}
+ */
 export var elements = {
   wall: 'x',
   food: '=',
   player: '-'
-}
+};
+
+
+
+/**
+ * the step direction north, south, west or east
+ * @type {Object}
+ */
+export var direction = {
+  n: { x: -1, y: 0 },
+  w: { x: 0, y: -1 },
+  e: { x: 0, y: 1 },
+  s: { x: 1, y: 0}
+};
+
