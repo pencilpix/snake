1c662a74dbb13171d3cbf5276a24be192af351c5
Mohamed hassan <pencilpix11@gmail.com>
2017-01-25 21:06:06 +0200

    Body: add pos property of x,y coordinate

---
+++ src/assets/js/es6/modules/snake-body.js
@@ -0,0 +1,9 @@
+/**
+ * snake body instances of rest of snake parts and its coordinates
+ * @param {Objects}  pos  Vector of x,y coordinates
+ */
+export class Body {
+  constructor(pos) {
+    this.pos = pos;
+  }
+}
