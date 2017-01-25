043cb46b4cadc427b9086afce6ec0994cd36be60
Mohamed hassan <pencilpix11@gmail.com>
2017-01-25 23:58:05 +0200

    test Snake: step, speed, size and direction

--- test/specs/snake.js
+++ test/specs/snake.js
@@ -13,4 +13,28 @@ describe('Snake class', () => {
     expect(snake.pos.x).toEqual(2, 'x is not equal to 2');
     expect(snake.pos.y).toEqual(3, 'y is not equal to 3');
   });
+
+  it('should have step property equal to 0.05', () => {
+    expect(snake.step).toEqual(0.05);
+  });
+  it('should have speed property of x = 0, y = 0', () => {
+    expect(snake.speed instanceof Vector).toBe(true);
+    expect(snake.speed.x).toEqual(0);
+    expect(snake.speed.y).toEqual(0);
+  });
+
+  it('should have size property of x = 1, y = 1', () => {
+    expect(snake.size instanceof Vector).toBe(true);
+    expect(snake.size.x).toEqual(1);
+    expect(snake.size.y).toEqual(1);
+  });
+
+  it('should have direction property with left direction true', () => {
+    Object.keys(snake.direction).forEach(dir => {
+      if(dir === 'left')
+        expect(snake.direction[dir]).toBe(true);
+      else
+        expect(snake.direction[dir]).toBe(false);
+    });
+  });
 });
