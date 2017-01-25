eab01e0d9cb1b5a6926ae0776cb57fb4e543c866
Mohamed hassan <pencilpix11@gmail.com>
2017-01-25 18:35:09 +0200

    level: width and height properties

--- src/assets/js/es6/modules/level.js
+++ src/assets/js/es6/modules/level.js
@@ -1,7 +1,11 @@
 /**
- * level
+ * Level: creates a level from a plan that describe
+ * current environment of the game
+ * @param {Array<String>}  plan  array of strings (nxm);
  */
 export class Level {
-  constructor() {
+  constructor(plan) {
+    this.width = plan[0].length;
+    this.height = plan.length;
   }
 }
