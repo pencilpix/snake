93dc43caa50b6532d8b4d19bc2c10f58f84a8f8a
Mohamed hassan <pencilpix11@gmail.com>
2017-01-25 21:12:41 +0200

    Food: add pos property of x, y coordinate

---
+++ src/assets/js/es6/modules/food.js
@@ -0,0 +1,9 @@
+/**
+ * food of the snake and its position x, y coordinates
+ * @param {Object}  Vector instance of x,y coordinates 
+ */
+export class Food {
+  constructor(pos) {
+    this.pos = pos;
+  }
+}
