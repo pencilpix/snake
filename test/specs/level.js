35e6f2410ed2dfd11ba3aadea7cbc273b7c17a12
Mohamed hassan <pencilpix11@gmail.com>
2017-01-25 18:44:25 +0200

    test level: should have grid property Array(nxm) as plan(nxm)

--- test/specs/level.js
+++ test/specs/level.js
@@ -16,4 +16,10 @@ describe('Level class', () => {
     expect(level.width).toEqual(utils.SIMPLE_PLAN[0].length);
   });
 
+  it('should have grid property Array(nxm) as plan(nxm)', () => {
+    expect(Array.isArray(level.grid)).toBe(true);
+    expect(level.grid.length).toEqual(utils.SIMPLE_PLAN.length);
+    expect(level.grid[0].length).toEqual(utils.SIMPLE_PLAN[0].length);
+  });
+
 });
