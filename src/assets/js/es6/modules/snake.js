bea88663e6cd900af3c8400fe1d2242bfca44aa2
Mohamed hassan <pencilpix11@gmail.com>
2017-01-25 20:59:35 +0200

    snake: pos property of x, y coordinates

---
+++ src/assets/js/es6/modules/snake.js
@@ -0,0 +1,9 @@
+/**
+ * Snake is the snake head and its position and size
+ * @param {Object} Vector instance of x, y coordinates
+ */
+export class Snake {
+  constructor(pos) {
+    this.pos = pos;
+  }
+}
