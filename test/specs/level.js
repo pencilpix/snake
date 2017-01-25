c5613539f8c024998591a219b3574b65181977e4
Mohamed hassan <pencilpix11@gmail.com>
2017-01-25 18:28:19 +0200

    test level: should have width(m) and height(n) of the plan array(nxm)

---
+++ test/specs/level.js
@@ -0,0 +1,18 @@
+import { Level }  from '../../src/assets/js/es5/modules/level';
+import * as utils from '../../src/assets/js/es5/modules/utils';
+
+describe('Level class', () => {
+  let level;
+
+  beforeAll(() => {
+    level = new Level(utils.SIMPLE_PLAN);
+  });
+
+  it('should has height equal to plan array length', () => {
+    expect(level.height).toEqual(utils.SIMPLE_PLAN.length);
+  });
+
+  it('should have width equal to plan item length', () => {
+    expect(level.width).toEqual(utils.SIMPLE_PLAN[0].length);
+  });
+});
