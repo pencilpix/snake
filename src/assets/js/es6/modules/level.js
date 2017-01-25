5c091830e8bca86ecf26b323ec0f8a05944c7408
Mohamed hassan <pencilpix11@gmail.com>
2017-01-25 22:30:26 +0200

    level: fix actors positions

--- src/assets/js/es6/modules/level.js
+++ src/assets/js/es6/modules/level.js
@@ -1,3 +1,14 @@
+import { Snake } from './snake';
+import { Body }  from './snake-body';
+import { Food }  from './food';
+import { Vector } from './vector';
+
+const ACTORS_TYPES = {
+  'o': Food,
+  's': Snake,
+  'z': Body
+};
+
 /**
  * Level: creates a level from a plan that describe
  * current environment of the game
@@ -8,16 +19,18 @@ export class Level {
     this.width = plan[0].length;
     this.height = plan.length;
     this.grid = [];
+    this.actors = [];
 
-    plan.forEach((line) => {
+    plan.forEach((line, i) => {
       let gridLine = [];
 
-      [].forEach.call(line, (ch) => {
+      [].forEach.call(line, (ch, k) => {
         let chType = null;
+        let Actor = ACTORS_TYPES[ch];
 
-        if(ch === 'x') {
-          chType = 'wall';
-        }
+        if(Actor) this.actors.push(new Actor(new Vector(k, i)));
+
+        if(ch === 'x') chType = 'wall';
 
         gridLine.push(chType);
       });
