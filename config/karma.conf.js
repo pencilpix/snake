0ee6fc4a6d6d5f05b7ec166a61279e51467011b5
Mohamed hassan <pencilpix11@gmail.com>
2017-06-05 04:31:16 +0200

    fix karma browser incase of travis

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -90,7 +90,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers:
     // https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: [(process.env.TRAVIS) ? 'Chrome_travis_ci' : 'Chrome'],
 
     customLaunchers: (process.env.TRAVIS) ? travisLauncher: chromeLauncher,
 
